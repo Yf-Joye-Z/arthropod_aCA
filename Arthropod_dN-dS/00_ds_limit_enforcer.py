b56f6cf72989e774f8eada07ac4ef6dfbdba96b8
Yifei "Joye" Zhou <yz937@duke.edu>
2025-02-23 16:37:05 -0500

    Update 00_ds_limit_enforcer.py

--- Arthropod_dN-dS/00_ds_limit_enforcer.py
+++ Arthropod_dN-dS/00_ds_limit_enforcer.py
@@ -1,24 +1,30 @@
 # This script removes all dS value that is lower than 0.0001 or greater than 2 from the analysis. It also removes all dN/dS values that are greater than 3.
+# The last part of this function needs to be hardcoded (name of the json and output name). 
+
 import json
 
 def filter_json(input_json_path, output_json_path):
     with open(input_json_path, 'r') as f:
         data = json.load(f)
-
+        
+    #Iterating through the json file for the correct key and values
     if "branch attributes" in data:
         branch_attitude = data["branch attributes"]
         if "0" in branch_attitude:
             keys_to_remove = []
             for key, value in branch_attitude["0"].items():
-               # print(value)
+                # moving keys with dS smaller than 0.0001 into the keys_to_remove list
                 if "dS" in value and value["dS"] < 0.0001:
                     keys_to_remove.append(key)
-                if "dS" in value and value["dS"] >2:
+                # moving keys with dN/dS greater than 3 into the keys_to_remove list
+                elif "dN" in value and value["dN"]/value["dS"] >3:
                     keys_to_remove.append(key)
-                if "dN" in value and value["dN"]/value["dS"] >3:
+                # moving keys with dS greater than 2 into the keys_to_remove list  
+                if "dS" in value and value["dS"] >2:
                     keys_to_remove.append(key)
             
             for key in keys_to_remove:
+                # remove all keys from the input json file that has matched with the keys within the "keys_to_remove" list
                 del branch_attitude["0"][key]
         else:
             print("no 0")
@@ -28,7 +34,7 @@ def filter_json(input_json_path, output_json_path):
     with open(output_json_path, 'w') as f:
         json.dump(data, f, indent=4)
 
-# Example usage
-input_json_path = "Extracellular_dn-ds.json"
-output_json_path = "output.json"
+# INSERT THE FILE NAME HERE ---> HARD CODED
+input_json_path = ""
+output_json_path = ""
 filter_json(input_json_path, output_json_path)
