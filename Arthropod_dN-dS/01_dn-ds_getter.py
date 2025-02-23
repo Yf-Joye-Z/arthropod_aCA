5d122839a0df49f15d958de75bc92cb08b6966dd
Yifei "Joye" Zhou <yz937@duke.edu>
2025-02-23 03:45:47 -0500

    Update and rename dn-ds_getter.py to 01_dn-ds_getter.py

--- Arthropod_dN-dS/01_dn-ds_getter.py
+++ Arthropod_dN-dS/01_dn-ds_getter.py
@@ -1,38 +1,38 @@
-# This codes extracts all MLE (omega)value from the output text from json_parser.py
+# This codes extracts all MLE (omega)value from the output json of json_parser.py
 
 import json
 from openpyxl import Workbook
 
 def process_json_file(input_file, output_file):
-    # Load JSON data
+    # Loading JSON file
     with open(input_file, 'r') as json_file:
         data = json.load(json_file)
     
-    # Create a new Excel workbook
+    # Creating an excel file
     wb = Workbook()
     ws = wb.active
     
     # Add headers
     ws.append(["Original Name", "MLE"])
 
-    # Iterate over keys within "branch attributes"->"0"
+    # Iterate over keys within "branch attributes" and "0".
     if "branch attributes" in data:
         branch_data = data["branch attributes"].get("0", {})
+        # Iterating over the species for MLE value
         for key, value in branch_data.items():
-            print(key)
             if "MLE" in value.get("Confidence Intervals", {}):
                 mle_value = value["Confidence Intervals"]["MLE"]
                 original_name = value.get("original name", "")
                 ws.append([original_name, mle_value])
-            else:
-                print("no MLE")
+            #else:
+                #print("no MLE")
 
-    # Save the workbook
+    # Saving the excel
     wb.save(output_file)
 
 # Input and output file paths
-input_file = 'output.json'
-output_file = 'Extracellular_omega.xlsx'
+input_file = ''
+output_file = ''
 
 # Process JSON file
 process_json_file(input_file, output_file)
